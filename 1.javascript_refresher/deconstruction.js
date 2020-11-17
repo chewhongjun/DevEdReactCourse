349f02b8d5ba4765e1d69488a6dbeb6e85dcd31f
hchew007@e.ntu.edu.sg <hchew007@e.ntu.edu.sg>
2020-11-18 03:32:59 +0800

    decon

---
+++ 1.javascript_refresher/deconstruction.js
@@ -0,0 +1,13 @@
+const user = {
+    name: "Ed",
+    lastName: "Dev",
+    age: 20
+};
+
+const { name, lastName, age } = user
+console.log(lastName)
+
+const numbers = [1, 2, 3, 4, 5]
+const [yol, lo] = numbers
+
+console.log(yol, lo)
